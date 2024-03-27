8b4ca0c077c4e7aa596bf86d3f79148fa0ec44d3
David <dmcfarland8031@gmail.com>
2024-03-27 17:37:15 -0600

    Moved to cmake and updated .gitignore

--- pointers.cpp
+++ pointers.cpp
@@ -1,19 +1,49 @@
 #include <iostream>
 
-class Practice_pointers {
-  int a_val = 0;
-public:Practice_pointers(){}
-  
+class Practicing_pointers {
+public:Practicing_pointers(){}
+  int a_val;
+  int static st_val;
+
+  Practicing_pointers(int val) {
+   const int a_val = val;
+  }
+  Practicing_pointers(Practicing_pointers &an_obj) {
+	a_val = an_obj.a_val;
+	st_val = an_obj.st_val;
+  }
 };
 
-int main () {
+void ptr_basics() {
+  int a_val = 420;
+  int *a_ptr = &a_val;
+  int **another_ptr = nullptr;
+  std::cout << "Integer Pointer memory location: " << a_ptr << std::endl;
+  std::cout << "Integer Pointer value: " << *a_ptr << std::endl;
 
-  int a = 1;
-  int *a_ptr = &a;
-  Practice_pointers obj;
-
-  Practice_pointers *pntr_obj = &obj; // pntr_obj contains the memory location of obj.
-  if (a_ptr >= 0) {
-	
-  }
+  std::cout << "Second Pointer memory location: " << another_ptr << std::endl;
+  std::cout << "Second Pointer value: " << **another_ptr << std::endl;
 }
+void null_ptrs() {
+  int a_num = 10;
+  int *make_ptr = &a_num;
+  int *second_ptr = nullptr;
+
+  std::nullptr_t *null_ptr_type;
+
+  std::cout << "Pointer contains a value: Expect 0\n" << second_ptr << std::endl;
+
+  if (null_ptr_type) {
+	std::cout << "Is this the same as make_ptr" << std::endl; // no
+	std::cout << "null_ptr_type: \n" << null_ptr_type << std::endl;
+  }
+  if (make_ptr) {
+	std::cout << "Make pointer contains a value" << std::endl;
+  }
+  
+}
+
+int main () {
+  null_ptrs();
+}
+
--- scratch1.cpp
+++ scratch1.cpp
@@ -1,35 +1,45 @@
 #include <iostream>
+#include <vector>
 
 class Person {
 public:
   Person() {
   } // default constructor
   bool employee;
+  double pay_rate;
   std::string name;
-
-  Person (bool an_employee, std::string a_name) { // paramaterized constructor
-	employee = an_employee;
+  const double hours_paid = 2080;
+  
+  Person (bool an_employee, double emp_pay_rate, std::string a_name) { // paramaterized constructor
+	employee =  an_employee;
 	name = a_name;
-
+	pay_rate = emp_pay_rate;
   }
 
   Person (Person &a_copy) { // copy constructor
 	employee = a_copy.employee;
 	name = a_copy.name;
+	pay_rate = a_copy.pay_rate;
   }
 
+  double salary() {
+	return pay_rate * hours_paid;
+  }
+  
   void display() {
 	if (employee == true) {
 	  std::cout << "Employee Name: " << name << std::endl;
+	  std::cout << "Employee Salary: " << salary() << std::endl;
 	} else {
 	  std::cout << "Person's Name: " << name << std::endl;
 	}
   }
 };
 
+
 int main () {
   Person first_person;
-  Person second_person(false, "David");
+  Person second_person(false, 20.00, "David");
   Person first_employee(first_person);
   Person &ref_to_person = first_person;
 
